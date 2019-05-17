de0e85d8500f7461e1d72caf7dfa07e1a46fe03b
Fred Skou <fred82e4@stud.kea.dk>
2019-05-17 12:47:53 +0200

    Fred

--- src/main/java/Repository/AdminRepo.java
+++ src/main/java/Repository/AdminRepo.java
@@ -1,7 +1,37 @@
 package Repository;
 
-public class AdminRepo {
-    public void confirmLogin(){
 
+import Config.MySQLConnection;
+import Service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.management.Query;
+import java.security.NoSuchAlgorithmException;
+import java.sql.*;
+@Repository
+public class AdminRepo {
+    AdminService admService;
+    Connection con;
+    MySQLConnection msc;
+
+    public AdminRepo(MySQLConnection msc){
+        this.msc=msc;
+        this.msc= (MySQLConnection) msc.create();
     }
+    public ResultSet confirmLogin() throws NoSuchAlgorithmException, SQLException {
+        String username = "Nils";
+        String password = admService.checkPassword();
+        String q =
+                "SELECT from stairwaydatabase.admin WHERE username = '" +username+ "AND password= '"+password+"'";
+        try {
+            ResultSet rs = new Query(q);
+            return rs;
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+    private
+
 }
--- src/main/java/Service/AdminService.java
+++ src/main/java/Service/AdminService.java
@@ -1,19 +1,71 @@
 package Service;
 
-import Models.Admin;
+import Repository.AdminRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class AdminService implements AdminServiceI {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-    /**
-     *
-     * @param admin
-     */
-    @Override
-    public Boolean confirmLogin(Admin admin) {
-        // TODO - implement AdminService.confirmLogin
-        throw new UnsupportedOperationException();
+@Service
+public class AdminService {
+    @Autowired
+    AdminRepo admRepo;
+
+             // En metode kalled "hashpassword" der har en String af typen password som parameter.
+            public static String hashPassword(String password) throws NoSuchAlgorithmException{
+            //Vi laver en ny instance af MessageDigest.
+            //getInstance("SHA-224") giver MessageDigest besked om hvilken Messagedigest vi vil have.
+            MessageDigest md = MessageDigest.getInstance("SHA-224");
+            // Updaterer md med de bytes vi vil have, password i det her tilfælde.
+            md.update(password.getBytes());
+            //Laver et byte array med de bytes fra md.digest.
+            byte[] b = md.digest();
+            //En Stringbuffer gør det muligt at indsætte ting i en String.
+            StringBuffer sb = new StringBuffer();
+            for(byte b1 : b){
+                //Vi fortæller vores StringBuffer hvad der skal lægges ind i vores String.
+                //Vi siger den skal lægger hexkode versionen af en integer ind.
+                // Jeg ved ikk helt hvad b1 & 0xff gør, men det skal være der.
+                sb.append(Integer.toHexString(b1 & 0xff));
+            }
+            //Vi kommer tilbage med en .toString version af vores StringBuffer object.
+            return sb.toString();
+        }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        AdminService adm = new AdminService();
+        //adm.kodeord();
+        adm.checkPassword();
     }
+    /*
+        public void kodeord() {
+            //Vi definerer passwordet, det kunne ogsp være indput.
+            String password = "Stairwaypassword1";
+            try {
+                //Vi printer hashPassword metoden ud med vores password som parameter.
+                System.out.println(hashPassword(password));
+
+            } catch (NoSuchAlgorithmException e) {
+                System.out.println(e);
+            }
+        }
+    */
+
+        public String checkPassword() throws NoSuchAlgorithmException {
+            //Vi tjekker lige og ser om det er det samme.
+            //Vi tager passwordet, hasher det og ser om det er det samme som er i vores database.
+            //Her udskifter vi selvfølgelig med SQL kald og hvad vi ellers har brug for.
+        String password = "Stairwaypassword1";//Indput fra hjemmeside.
+        String hashedPassword = hashPassword("Stairwaypassword1");
+        if (hashPassword(password).equals(hashedPassword)){
+            System.out.println("Correct Password");
+        }
+        else {
+            System.out.println("Incorrect Password");
+        }
+            return password;
+        }
+
 
 }
