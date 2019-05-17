9d2b720a247644ffb8db3b252575e07a1abc18e0
Isaksj <44366691+Isaksj@users.noreply.github.com>
2019-05-17 11:37:42 +0200

    Config med kodeord til database

--- src/main/java/Config/MySQLConnection.java
+++ src/main/java/Config/MySQLConnection.java
@@ -1,4 +1,45 @@
 package Config;
+import org.springframework.context.annotation.Configuration;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+@Configuration
 public class MySQLConnection {
+
+    //Her ligger oplysningerne brugt til at forbinde til vores database.
+    // De er gemt som datatypen String og bruges af metoderne.
+    private String db_host = "den1.mysql2.gear.host";
+    private String db_database = "stairwaydatabase";
+    private String db_user = "stairwaydatabase";
+    private String db_password = "Zo1zeb?zSf!6";
+    private Connection con;
+
+    // constructor
+    public MySQLConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // create db connection. returns null on failure
+    public Connection create() {
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://"+db_host+"/"+db_database, db_user, db_password);
+            return con;
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public void connClose() throws SQLException {
+        this.con.close();
+    }
+
+
 }
