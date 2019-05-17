46c79d3385f7a9c9233654a3f7a0a153c27ac777
Immanuellllllll <44088357+Immanuellllllll@users.noreply.github.com>
2019-05-17 11:30:53 +0200

    ms

--- src/main/java/Service/MemberService.java
+++ src/main/java/Service/MemberService.java
@@ -1,4 +1,34 @@
 package Service;
 
+import Models.Member;
+
+import java.util.List;
+
 public class MemberService {
+
+    public void registerMember(Member member) {
+        // TODO - implement MemberService.registerMember
+        throw new UnsupportedOperationException();
+    }
+
+
+    public void editMember(Member member) {
+        // TODO - implement MemberService.editMember
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param memberid
+     */
+    public void deleteMember(int memberid) {
+        // TODO - implement MemberService.deleteMember
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Member> viewMembers() {
+        // TODO - implement MemberService.viewMembers
+        throw new UnsupportedOperationException();
+    }
+
 }
