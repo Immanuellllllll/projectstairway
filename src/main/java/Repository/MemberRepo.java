a153247cd57cadc0b70400d939d81ebf36825d71
Isaksj <44366691+Isaksj@users.noreply.github.com>
2019-05-17 12:46:52 +0200

    member er under konstruktion

--- src/main/java/Repository/MemberRepo.java
+++ src/main/java/Repository/MemberRepo.java
@@ -4,10 +4,7 @@ import Config.MySQLConnection;
 import Models.Member;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 @Repository
 public class MemberRepo {
@@ -21,31 +18,44 @@ public class MemberRepo {
         //Lets load the driver
 
     }
-    /**
-     *
-     * @param member
-     */
-    public void registerMember(Member member) {
-        // TODO - implement MemberRepository.registerMember
-        throw new UnsupportedOperationException();
+
+    public void createMember(int memberId,String firstName,String lastName,String street,String postalcode, String city, String email) throws SQLException {
+        String q = "INSERT INTO members (memberid, firstname, lastname, street, postalcode, city, email)"+" VALUES (?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement preparedStmt = con.prepareStatement(q);
+        preparedStmt.setInt(1, memberId);
+        preparedStmt.setString (2, firstName);
+        preparedStmt.setString (3, lastName);
+        preparedStmt.setString (4, street);
+        preparedStmt.setString (5, postalcode);
+        preparedStmt.setString (6, city);
+        preparedStmt.setString (7, email);
+        preparedStmt.execute();
+        preparedStmt.close();
     }
 
-    /**
-     *
-     * @param member
-     */
-    public void editMember(Member member) {
-        // TODO - implement MemberRepository.editMember
-        throw new UnsupportedOperationException();
+    public void editMember(int memberId,String firstName,String lastName,String street,String postalcode, String city, String email) throws SQLException {
+        String q = "UPDATE members SET(memberid, firstname, lastname, street, postalcode, city, email)"+" VALUES (?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement preparedStmt = con.prepareStatement(q);
+        preparedStmt.setInt(1, memberId);
+        preparedStmt.setString (2, firstName);
+        preparedStmt.setString (3, lastName);
+        preparedStmt.setString (4, street);
+        preparedStmt.setString (5, postalcode);
+        preparedStmt.setString (6, city);
+        preparedStmt.setString (7, email);
+        preparedStmt.execute();
+        preparedStmt.close();
     }
 
-    /**
-     *
-     * @param memberid
-     */
-    public void deleteMember(int memberid) {
-        // TODO - implement MemberRepository.deleteMember
-        throw new UnsupportedOperationException();
+
+
+
+    public void deleteMember(int memberid) throws SQLException {
+       String q= "DELETE FROM members"+"WHERE memberid="+"(?)";
+        PreparedStatement preparedStatement = con.prepareStatement(q);
+        preparedStatement.setInt(1,memberid);
+        preparedStatement.execute();
+        preparedStatement.close();
     }
 
     //Metoden prøver at sende en SQL sætning til databasen og lykkedes det sender den et resultset af alle patienter tilbage.
@@ -59,7 +69,6 @@ public class MemberRepo {
                 System.out.println(e);
             }
             return null;
-        // TODO - implement MemberRepository.viewMembers
     }
 
     //Metoden beder om en String og returnerer en ordre til at udføre den i databasen.
--- src/main/java/Service/MemberService.java
+++ src/main/java/Service/MemberService.java
@@ -1,32 +1,32 @@
 package Service;
 
 import Models.Member;
+import Repository.MemberRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 @Service
-public class MemberService implements MemberServiceI {
+public class MemberService {
+    @Autowired
+    MemberRepo pr;
 
-    @Override
-    public void registerMember(Member member) {
-        // TODO - implement MemberService.registerMember
-        throw new UnsupportedOperationException();
+    public void registerMember(Member member) throws SQLException {
+        pr.createPatient(patient.getCpr(),patient.getFirst_name(),patient.getLast_name(),patient.getAge(),patient.getAddress(),patient.getDob(),patient.getHeight(),patient.getWeight(),patient.isPenicillinallergy(),patient.isLactoseallergy(),patient.isGlutenallergy());
     }
 
 
-    @Override
     public void editMember(Member member) {
         // TODO - implement MemberService.editMember
         throw new UnsupportedOperationException();
     }
 
-    @Override
     public void deleteMember(int memberid) {
         // TODO - implement MemberService.deleteMember
         throw new UnsupportedOperationException();
     }
 
-    @Override
     public List<Member> viewMembers() {
         // TODO - implement MemberService.viewMembers
         throw new UnsupportedOperationException();
