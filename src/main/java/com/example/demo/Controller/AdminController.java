bc35ba97dc26b9433312ab4fc7aed43357745367
Isaksj <44366691+Isaksj@users.noreply.github.com>
2019-05-21 13:14:06 +0200

    Login

--- src/main/java/com/example/demo/Controller/AdminController.java
+++ src/main/java/com/example/demo/Controller/AdminController.java
@@ -1,11 +1,37 @@
 package com.example.demo.Controller;
 
 import com.example.demo.Models.Admin;
-import com.example.demo.Repository.AdminRepo;
+import com.example.demo.Service.AdminServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 
 @Controller
 public class AdminController {
+    @Autowired
+    AdminServiceI As;
+
+    @GetMapping("/")
+    public String Login (Model model){
+        model.addAttribute("admin", new Admin());
+        return "Login";
+    }
+
+    @PostMapping("/Login")
+    public String confirmLogin (@ModelAttribute Admin admin) throws SQLException, NoSuchAlgorithmException {
+        try {
+            As.confirmLogin(admin);
+            }
+        catch(Exception E) {
+            return "Login";
+            }
+        return "backoffice";
+    }
 
 }
